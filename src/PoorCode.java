2e17f7db91086a05fdf0fb816d106935afa922c2
Justice <unankajustice@gmail.com>
2024-01-24 13:06:35 +0000

    first commit

---
+++ src/PoorCode.java
@@ -0,0 +1,243 @@
+import java.util.Arrays;
+
+//import static com.sun.tools.classfile.Module_attribute.RequiresEntry.length;
+
+// Poor Code class
+public class PoorCode {
+
+    private static double[] sorted;
+    private static double sum;
+
+    public static void main(String[] args) {
+
+        greetings("Mr", "Sergey", "Ovchinnik");
+
+        System.out.println();
+
+        int[] arr = new int[] {5, 8, 3, 4, 2, 9, 6};
+        System.out.println("Top even is " + locateTopEven(arr));
+
+        System.out.println();
+
+        System.out.println("evaluateConditions(false, false, -1) = " + evaluateConditions(false, false, -1));
+        System.out.println("evaluateConditions(false, true, -1) = " + evaluateConditions(false, true, -1));
+        System.out.println("evaluateConditions(true, false, -1) = " + evaluateConditions(true, false, -1));
+        System.out.println("evaluateConditions(true, true, -1) = " + evaluateConditions(true, true, -1));
+        System.out.println("evaluateConditions(false, false, 0) = " + evaluateConditions(false, false, 0));
+        System.out.println("evaluateConditions(false, true, 0) = " + evaluateConditions(false, true, 0));
+        System.out.println("evaluateConditions(true, false, 0) = " + evaluateConditions(true, false, 0));
+        System.out.println("evaluateConditions(true, true, 0) = " + evaluateConditions(true, true, 0));
+        System.out.println("evaluateConditions(false, false, 1) = " + evaluateConditions(false, false, 1));
+        System.out.println("evaluateConditions(false, true, 1) = " + evaluateConditions(false, true, 1));
+        System.out.println("evaluateConditions(true, false, 1) = " + evaluateConditions(true, false, 1));
+        System.out.println("evaluateConditions(true, true, 1) = " + evaluateConditions(true, true, 1));
+
+        System.out.println();
+
+        double[] arr2 = new double[] {3.0, 4.0, 3.5, 4.0, 6.0, 5.0, 5.5, 6.6, 6.1};
+        printStats(arr2);
+
+        System.out.println();
+
+        System.out.println(intArrayToString(new int[] {1, 2, 3, 4, 5}));
+
+        System.out.println();
+
+        System.out.println(casing("Big Sergio Comp8860"));
+
+    }
+
+    /*
+     * Method grrtings
+     *
+     * Takes in three string parameters and prints out the greeting
+     *
+     * First parameter serves as the title, second as the first name and second is the surname
+     *
+     */
+    public static void greetings(String title, String firstName, String secondName){
+        System.out.println("Hello, " + title + " " + firstName + " " + secondName);
+    }
+
+    public static int locateTopEven(int[] arr){
+        int topEven = Integer.MIN_VALUE, indexOfTopEven = 0;
+        for(int i=0; i< arr.length; i++){
+            if(arr[i] % 2 == 0){
+                if( arr[i] > topEven){
+                    topEven = arr[i];
+                }
+                indexOfTopEven = i;
+            }
+        }
+        return indexOfTopEven;
+    }
+
+
+    public static boolean evaluateConditions(boolean a, boolean b, int c) {
+        if(c >0) {
+            return a != b;
+        }
+        if(c < 0) {
+            return a != b;
+        }
+        return false;
+    }
+
+    /*
+     * Method printStats
+     */
+
+    public static void printStats(double[] arr) {
+        double length = (double) arr.length;
+        System.out.println("Length = " + length);
+    }
+
+    /*
+     * Method the sumOfArray
+     */
+    public static void sumOfArray(double[] arr) {
+        double sum = 0.0;
+        for (int i = 0; i < arr.length; i++)
+            sum += arr[i];
+        System.out.println("Sum = " + sum);
+    }
+
+    public static void mean(double[] arr) {
+        double n = (double) arr.length;
+        double mean = sum / n;
+        System.out.println("Mean = " + mean);
+    }
+    public static void median(double[] arr) {
+        double[] sorted = b(arr);
+        double median;
+        if (sorted.length % 2 == 1) {
+            median = sorted[sorted.length / 2];
+        } else {
+            median = (sorted[sorted.length / 2 - 1] + sorted[sorted.length / 2]) / 2.0;
+        }
+        System.out.println("Median = " + median);
+    }
+
+    public static void mode(double[] arr) {
+        double mode = sorted[0];
+        int topOccurrences = 0;
+        double currentNumber = sorted[0];
+        int occurrences = 0;
+        for (int i = 0; i < sorted.length; i++) {
+            if (sorted[i] == currentNumber) {
+                occurrences++;
+                if (occurrences > topOccurrences) {
+                    topOccurrences = occurrences;
+                    mode = currentNumber;
+                }
+            } else {
+                currentNumber = sorted[i];
+                occurrences = 1;
+            }
+        }
+
+        System.out.println("Mode = " + mode);
+    }
+    public static void variance(double[] arr) {
+        double variance = 0.0;
+
+        for(int i = 0; i < arr.length; i++) {
+            double mean = 0;
+            double diff = arr[i] - mean;
+            variance += diff * diff;
+        }
+        variance /= length;
+        System.out.println("Variance = " + variance);
+        double standardDeviation = Math.sqrt(variance);
+        System.out.println("Standard Deviation = " + standardDeviation);
+
+    }
+
+    public static double[] sortArrays(double[] arr) {
+        double[] sortingArr = Arrays.copyOf(arr, arr.length);
+        for(int i = 0; i < sortingArr.length - 1; i++)
+            for(int j = i + 1; j < sortingArr.length; j++)
+                if(sortingArr[i] > sortingArr[j]) {
+                    double temp = sortingArr[i];
+                    sortingArr[i] = sortingArr[j];
+                    sortingArr[j] = temp;
+                }
+        return sortingArr;
+    }
+
+    // True for 2, 3, 5, 7, 11, 13 ...
+    // False for 1, 4, 6, 8, 9, 10, 12 ...
+    public static boolean numberSorting(int n) {
+        if(n <= 1)
+            return false;
+        for(int i = 2; i < n; i++)
+            if(n % i == 0)
+                return false;
+        return true;
+    }
+
+    // Converts integer array to string
+    public  static String intArrayToString(int [] arr)
+    {
+        if(arr == null)		return "null"; // if null then "null"
+        String out = "[";
+        for(int i =   0;i<arr.length; i++) {
+            if(i > 0)
+                out = out + ", "; { // add comma when necessary
+                out = out + arr[i];
+            }}  /* TODO: fix this */
+        out = out + "]";
+        // Close the array
+        return out; // return out;
+    }
+
+    // Big Sergio Comp8860 -> bIG sERGIO cOMP8860
+    public static String casing(String s) {
+        String out = "";
+        for(int i = 0; i < s.length(); i++) {
+            if(Character.isUpperCase(s.charAt(i))) {
+                out = out + Character.toLowerCase(s.charAt(i));
+            }
+            else
+            if(Character.isLowerCase(s.charAt(i)))
+                out = out + Character.toUpperCase(s.charAt(i));
+            else
+                out = out + s.charAt(i);
+        }
+        return out;
+    }
+
+    public static int howMany(boolean f, boolean t, boolean k, boolean z) {
+        if(f == true) {
+            if(k == false) {
+                if(z && t)
+                    return 3;
+                if(z || t)
+                    return 2;
+                return 1;
+            }
+            else {
+                if(z == true) {
+                    if(t) return 4;
+                    else return 3;
+                }
+                if(t == true) {
+                    if(z) return 4;
+                    else return 3;
+                }
+                else return 2;
+            }
+        }
+        if(k && t && z) {
+            return 3;
+        }
+        if(k || z || t) {
+            int number = 0;
+            if(k && z) number = number + 2;
+            else if(z || k) number ++;
+            number+=(t?1:0);
+            return number;
+        }
+        return 0;
+    }
+}
